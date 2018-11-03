472982a91a0f37126e04d91eae4d14a1e2f8e45f
Lavesh Panjwani <lpanjwani@outlook.com>
2018-11-03 14:53:38 +0400

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -3,96 +3,108 @@
 int
 main ()
 {
+  int flag = 0;
+  char option;
 
-  float value;
-  int dollar, cents;
+  while(flag == 0){
 
-  printf ("Enter Value: ");
-  scanf ("%f", &value);
+      float value;
+      int dollar, cents;
 
-  dollar = (int) value;
-  cents = (int) (((value - dollar) * 100) + 0.5);
+      printf ("\n Enter Value: ");
+      scanf ("%f", &value);
 
-  // Dollar Section
+      dollar = (int) value;
+      cents = (int) (((value - dollar) * 100) + 0.5);
 
-  int one_dollar, two_dollar, five_dollar, ten_dollar, twenty_dollar,
-    fifty_dollar, hundred_dollar;
+      // Dollar Section
 
-  one_dollar = dollar / 1;
-  two_dollar = dollar / 2;
-  five_dollar = dollar / 5;
-  ten_dollar = dollar / 10;
-  twenty_dollar = dollar / 20;
-  fifty_dollar = dollar / 50;
-  hundred_dollar = dollar / 100;
+      int one_dollar, two_dollar, five_dollar, ten_dollar, twenty_dollar,
+	fifty_dollar, hundred_dollar;
 
-  if (hundred_dollar >= 1)
-    {
-      printf ("%d - $100 Notes \n", hundred_dollar);
+      one_dollar = dollar / 1;
+      two_dollar = dollar / 2;
+      five_dollar = dollar / 5;
+      ten_dollar = dollar / 10;
+      twenty_dollar = dollar / 20;
+      fifty_dollar = dollar / 50;
+      hundred_dollar = dollar / 100;
 
-    }
-  if (fifty_dollar >= 1)
-    {
-      printf ("%d - $50 Notes \n", fifty_dollar);
-
-    }
-  if (twenty_dollar >= 1)
-    {
-      printf ("%d - $20 Notes \n", twenty_dollar);
-
-    }
-  if (ten_dollar >= 1)
-    {
-      printf ("%d - $10 Notes \n", ten_dollar);
-
-    }
-  if (five_dollar >= 1)
-    {
-      printf ("%d - $5 Notes \n", five_dollar);
-
-    }
-  if (two_dollar >= 1)
-    {
-      printf ("%d - $2 Notes \n", two_dollar);
-
-    }
-  if (one_dollar >= 1)
-    {
-      printf ("%d - $1 Notes \n", one_dollar);
-
-    }
-
-
-  // Cents Section
-  if (cents >= 5 && cents <= 95 && cents % 5 == 0)
-    {
-      int five, ten, twenty, fifty;
-      five = value / 5;
-      ten = value / 10;
-      twenty = value / 25;
-      fifty = fifty / 50;
-
-      if (fifty >= 1)
+      if (hundred_dollar >= 1)
 	{
-	  printf ("%d - 50 Cents Coins \n", fifty);
+	  printf ("%d - $100 Notes \n", hundred_dollar);
 
 	}
-      if (twenty >= 1)
+      if (fifty_dollar >= 1)
 	{
-	  printf ("%d - 20 Cents Coins \n", twenty);
+	  printf ("%d - $50 Notes \n", fifty_dollar);
 
 	}
-      if (ten >= 1)
+      if (twenty_dollar >= 1)
 	{
-	  printf ("%d - 10 Cents Coins \n", ten);
+	  printf ("%d - $20 Notes \n", twenty_dollar);
 
 	}
-      if (five >= 1)
+      if (ten_dollar >= 1)
 	{
-	  printf ("%d - 5 Cents Coins", five);
+	  printf ("%d - $10 Notes \n", ten_dollar);
+
+	}
+      if (five_dollar >= 1)
+	{
+	  printf ("%d - $5 Notes \n", five_dollar);
+
+	}
+      if (two_dollar >= 1)
+	{
+	  printf ("%d - $2 Notes \n", two_dollar);
+
+	}
+      if (one_dollar >= 1)
+	{
+	  printf ("%d - $1 Notes \n", one_dollar);
+
 	}
 
+
+      // Cents Section
+      if (cents >= 5 && cents <= 95 && cents % 5 == 0)
+	{
+	  int five, ten, twenty, fifty;
+	  five = value / 5;
+	  ten = value / 10;
+	  twenty = value / 25;
+	  fifty = fifty / 50;
+
+	  if (fifty >= 1)
+	    {
+	      printf ("%d - 50 Cents Coins \n", fifty);
+
+	    }
+	  if (twenty >= 1)
+	    {
+	      printf ("%d - 20 Cents Coins \n", twenty);
+
+	    }
+	  if (ten >= 1)
+	    {
+	      printf ("%d - 10 Cents Coins \n", ten);
+
+	    }
+	  if (five >= 1)
+	    {
+	      printf ("%d - 5 Cents Coins \n", five);
+	    }
+
+	}
+      printf ("Do you want to continue ? Press Y to Continue, N to Exit. \n Enter Desired Option: ");
+      scanf (" %c", &option);
+      if (option == 'N' || option == 'n')
+	    {
+	        flag = 1;
+	    }
     }
 
   return 0;
 }
+
